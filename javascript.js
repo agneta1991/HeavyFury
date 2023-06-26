4eaec50ecdc6a57964949020d7a23041b4625c94
agneta1991 <pupienytea@gmail.com>
2023-06-27 00:59:09 +0300

    adding content to about page

--- javascript.js
+++ javascript.js
@@ -1,1 +1,28 @@
-console.log('im working');
+let seeMore = document.getElementById('see-more');
+
+seeMore.addEventListener('touchstart', seeMoreFunction);
+
+function seeMoreFunction(){
+let headline = document.getElementById('dynamic-headliners');
+
+let thirdHead = document.createElement('div');
+thirdHead.className = 'thirdBand band';
+/*thirdHead.innerHTML =*/
+headline.appendChild('thirdHead');
+
+let fourthHead = document.createElement('div');
+fourthHead.className = 'fourthBand band';
+fourthHead.innerHTML = 
+headline.appendChild('fourthHead');
+
+let fifthHead = document.createElement('div');
+fifthHead.className = 'fifthBand band';
+fifthHead.innerHTML = 
+headline.appendChild('fifthHead');
+
+let sixthHead = document.createElement('div');
+sixthHead.className = 'sixthBand band';
+sixthHead.innerHTML =
+headline.appendChild('sixthHead');
+
+}
